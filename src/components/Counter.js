920d65c40a207190850309c2d2be87f6c5b895ab
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-09 13:09:57 +0530

    feat : add toggle functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,11 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   const counter = useSelector((state) => state.counter);
+  const showToggle = useSelector((state) => state.toggle);
   const dispatch = useDispatch();
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -10,13 +13,17 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
+  const increasesHandler = () => {
+    dispatch({ type: "increases", amount: 5 });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showToggle && <div className={classes.value}>{counter}</div>}
       <div className="counter">
         <button onClick={incrementHandler}>increment</button>
+        <button onClick={increasesHandler}>Increases</button>
         <button onClick={decrementHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, toggle: false }, action) => {
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return { ...state, counter: state.counter + 1 };
   }
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    return { ...state, counter: state.counter - 1 };
   }
+  if (action.type === "increases") {
+    return { ...state, counter: state.counter + action.amount };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      toggle: !state.toggle,
+    };
+  }
+
   return state;
 };
 
